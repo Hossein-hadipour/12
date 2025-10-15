450c51b76c56d63d4822fd8e3c04b29ca9408d64
Hossein-hadipour <goddoers@gmail.com>
2025-10-15 21:12:35 +0330

    Delete data.js

--- data.js
+++
@@ -1,5 +0,0 @@
-﻿const users = [
-  { username: "super1", password: CryptoJS.SHA256("12345").toString(), role: "superadmin" },
-  { username: "admin1", password: CryptoJS.SHA256("1111").toString(), role: "admin" },
-  { username: "user1", password: CryptoJS.SHA256("0000").toString(), role: "user" }
-];
